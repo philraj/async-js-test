f5dd5792f75bddbe5f349d7909371687806cf518
Phil Rajchgot <tophil@outlook.com>
2016-02-09 22:31:02 -0500

    Adding iterator test

--- async-test.js
+++ async-test.js
@@ -11,7 +11,7 @@ beginning()
 function beginning () {
     prompt.get(['input'], function (err, result) {
         if (err) return log(err);
-        
+
         switch (result.input) {
             case 'forever':
                 async.forever(storeInput)
@@ -23,8 +23,20 @@ function beginning () {
                     beginning
                 )
                 break
-            case 'auto':
-                //add auto test here
+            case 'iterator':
+                var iterator = async.iterator([
+                    function () { log("Hello") },
+                    function () { log("World") },
+                    function () { log("This") },
+                    function () { log("is an") },
+                    function () { log("iterator!") }
+                ])
+                iterator = iterator()
+                iterator = iterator()
+                iterator = iterator()
+                iterator = iterator()
+                iterator()
+                beginning()
                 break
             case 'quit':
                 return log("Goodbye!")
@@ -37,29 +49,29 @@ function beginning () {
 function storeInput (callback) {
     prompt.get(['input'], function (err, result) {
         if (err) return log(err)
-        
+
         if (result.input === 'stop') {
             beginning()
         }
         else {
             inputArray.push(result.input)
             console.log(inputArray)
-            
+
             callback()
         }
-    })    
+    })
 }
 
 function displayInput (callback) {
     prompt.get(['input'], function (err, result) {
         if (err) return log(err)
-        
+
         if (result.input === 'stop') {
             stopDisplayInput = true;
         }
-        
+
         log(result.input)
-        
+
         callback()
     })
 }
